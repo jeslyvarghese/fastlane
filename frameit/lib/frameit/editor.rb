2f8cfed48d04a95e6a4ddb374451d5b441e95088
jesly <jesly.varghese@storytel.com>
2021-03-18 15:37:42 +0100

    Replaced with pango

--- frameit/lib/frameit/editor.rb
+++ frameit/lib/frameit/editor.rb
@@ -434,8 +434,7 @@ module Frameit
           i.pointsize(actual_font_size(key))
           i.interline_spacing(interline_spacing) if interline_spacing
           i.fill(@config[key.to_s]['color'])
-          i.direction("right-to-left") if ["ar", "arc", "dev", "fa", "ha", "he", "khw", "ks", "ku", "ps", "ur", "yi"].include? screenshot.language
-          i << "label:" << "'#{text}'"
+          i << "pango" << "label:" << "'#{text}'"
         end
 
         results[key] = text_image
